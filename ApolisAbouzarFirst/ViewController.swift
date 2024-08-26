e4c776960eeca1581a183352a9aa26717ad956dc
Abouzar <abouzarmoradian1@gmail.com>
2024-08-26 13:15:35 -0400

    added a button and a label inside viewController

--- ApolisAbouzarFirst/ViewController.swift
+++ ApolisAbouzarFirst/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     
     let inputArray: [Int] = [1, 4, 2, 1, 4, 5, 3, 4, 4, 8, 7, 7]
     var outputArray: [Int] = []
+    var label = UILabel()
+    var button = UIButton()
     
     
     
@@ -23,7 +25,33 @@ class ViewController: UIViewController {
         printName(input: Int.random(in: 0...100))
         secondLargest(input: inputArray)
         
+        label.backgroundColor = .blue
+        label.textColor = .white
+        label.textAlignment = .center
+        label.frame = CGRect(x: Int(view.frame.width)/2 - Int(view.frame.width)/4, y: Int(view.frame.height)/2 - Int(view.frame.width)/4, width: Int(view.frame.width)/2, height: Int(view.frame.width)/2)
+        view.addSubview(label)
         
+        button.backgroundColor = .gray
+        button.setTitle("Display", for: .normal)
+        button.frame = CGRect(x: Int(view.frame.width)/2 - Int(view.frame.width)/4, y: Int(view.frame.height)/2 + Int(view.frame.width)/4 + 20, width: Int(view.frame.width)/2, height: Int(view.frame.width)/4)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        view.addSubview(button)
+        
+        
+    }
+    
+    @objc func buttonAction(_ sender: UIButton){
+        
+        if let buttonTitle = sender.currentTitle, buttonTitle == "Display"{
+            sender.setTitle("Hide", for: .normal)
+        }else{
+            sender.setTitle("Display", for: .normal)
+        }
+        if let text = label.text, text == "Hello!" {
+            label.text = ""
+        }else{
+            label.text = "Hello!"
+        }
     }
     
     func removeDuplicate() {
